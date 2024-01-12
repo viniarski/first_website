1f586a52a76a73fd643c151d29b77f682f722cfa
Michal <viniarski@gmail.com>
2024-01-12 21:26:55 +0000

    fixed bugs in dark mode

--- app.js
+++ app.js
@@ -1,47 +1,75 @@
-let button_1 = document.getElementById("button_1")
+let button_1 = document.getElementById("button_1");
+let button_2 = document.getElementById("button_2");
+let button_3 = document.getElementById("button_3");
 
-function changeColor() {
-    button_1.style.background = "white"
-    button_1.style.color = "black"
+function handleButtonHover_1(event) {
+    if (event.type === 'mouseover') {
+        if (document.body.classList.contains('dark_mode')) {
+            button_1.style.background = "white";
+            button_1.style.color = "rgba(0, 0, 0, 1)";
+        } else {
+            button_1.style.background = "black";
+            button_1.style.color = "rgba(255, 255, 255, 1)";
+        }
+    } else {
+        if (document.body.classList.contains('dark_mode')) {
+            button_1.style.background = "none";
+            button_1.style.color = "white";
+        } else {
+            button_1.style.background = "none";
+            button_1.style.color = "black";
+        }
+    }
 }
 
-function changeColorBack() {
-    button_1.style.background = "none"
-    button_1.style.color = "white"
+function handleButtonHover_2(event) {
+    if (event.type === 'mouseover') {
+        if (document.body.classList.contains('dark_mode')) {
+            button_2.style.background = "white";
+            button_2.style.color = "rgba(0, 0, 0, 1)";
+        } else {
+            button_2.style.background = "black";
+            button_2.style.color = "rgba(255, 255, 255, 1)";
+        }
+    } else {
+        if (document.body.classList.contains('dark_mode')) {
+            button_2.style.background = "none";
+            button_2.style.color = "white";
+        } else {
+            button_2.style.background = "none";
+            button_2.style.color = "black";
+        }
+    }
 }
 
-button_1.addEventListener('mouseover', changeColor);
-button_1.addEventListener('mouseout', changeColorBack);
-
-let button_2 = document.getElementById("button_2")
-
-function changeColor2() {
-    button_2.style.background = "white"
-    button_2.style.color = "black"
+function handleButtonHover_3(event) {
+    if (event.type === 'mouseover') {
+        if (document.body.classList.contains('dark_mode')) {
+            button_3.style.background = "white";
+            button_3.style.color = "rgba(0, 0, 0, 1)";
+        } else {
+            button_3.style.background = "black";
+            button_3.style.color = "rgba(255, 255, 255, 1)";
+        }
+    } else {
+        if (document.body.classList.contains('dark_mode')) {
+            button_3.style.background = "none";
+            button_3.style.color = "white";
+        } else {
+            button_3.style.background = "none";
+            button_3.style.color = "black";
+        }
+    }
 }
 
-function changeColorBack2() {
-    button_2.style.background = "none"
-    button_2.style.color = "white"
-}
+button_1.addEventListener('mouseover', handleButtonHover_1);
+button_1.addEventListener('mouseout', handleButtonHover_1);
 
-button_2.addEventListener('mouseover', changeColor2);
-button_2.addEventListener('mouseout', changeColorBack2);
+button_2.addEventListener('mouseover', handleButtonHover_2);
+button_2.addEventListener('mouseout', handleButtonHover_2);
 
-let button_3 = document.getElementById("button_3")
-
-function changeColor3() {
-    button_3.style.background = "white"
-    button_3.style.color = "black"
-}
-
-function changeColorBack3() {
-    button_3.style.background = "none"
-    button_3.style.color = "white"
-}
-
-button_3.addEventListener('mouseover', changeColor3);
-button_3.addEventListener('mouseout', changeColorBack3);
+button_3.addEventListener('mouseover', handleButtonHover_3);
+button_3.addEventListener('mouseout', handleButtonHover_3);
 
 let top_nav_1 = document.getElementById('nav_1')
 
@@ -116,7 +144,7 @@ let button = document.querySelector('button')
 let forestSection_1 = document.querySelector('.forest_1');
 let forestSection_2 = document.querySelector('.forest_2');
 let forestSection_3 = document.querySelector('.forest_3');
-let navLinks = document.querySelectorAll('nav a, .button');
+let colors = document.querySelectorAll('nav a, .button, .name');
 
 function toggleDarkLightMode() {
     if (document.body.classList.contains('dark_mode')) {
@@ -126,7 +154,7 @@ function toggleDarkLightMode() {
         forestSection_3.style.backgroundImage = "url('./photos/dark_mode_forest_3.avif')";
         document.body.classList.remove('dark_mode');
         document.body.classList.add('light_mode');
-        navLinks.forEach(link => {
+        colors.forEach(link => {
             link.style.color = 'black';
             if (link.classList.contains('button')) {
                 link.style.borderColor = 'black';
@@ -139,7 +167,7 @@ function toggleDarkLightMode() {
         forestSection_3.style.backgroundImage = "url('./photos/forest_3.avif')";
         document.body.classList.remove('light_mode');
         document.body.classList.add('dark_mode');
-        navLinks.forEach(link => {
+        colors.forEach(link => {
             link.style.color = 'white';
             if (link.classList.contains('button')) {
                 link.style.borderColor = 'white';
