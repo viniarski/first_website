bcfbfcf249f0eb1dd691add2cd342e629ece0f13
Michal <viniarski@gmail.com>
2024-01-12 14:39:36 +0000

    add javascript events

---
+++ app.js
@@ -0,0 +1,110 @@
+let button_1 = document.getElementById("button_1")
+
+function changeColor() {
+    button_1.style.background = "white"
+    button_1.style.color = "black"
+}
+
+function changeColorBack() {
+    button_1.style.background = "none"
+    button_1.style.color = "white"
+}
+
+button_1.addEventListener('mouseover', changeColor);
+button_1.addEventListener('mouseout', changeColorBack);
+
+let button_2 = document.getElementById("button_2")
+
+function changeColor2() {
+    button_2.style.background = "white"
+    button_2.style.color = "black"
+}
+
+function changeColorBack2() {
+    button_2.style.background = "none"
+    button_2.style.color = "white"
+}
+
+button_2.addEventListener('mouseover', changeColor2);
+button_2.addEventListener('mouseout', changeColorBack2);
+
+let button_3 = document.getElementById("button_3")
+
+function changeColor3() {
+    button_3.style.background = "white"
+    button_3.style.color = "black"
+}
+
+function changeColorBack3() {
+    button_3.style.background = "none"
+    button_3.style.color = "white"
+}
+
+button_3.addEventListener('mouseover', changeColor3);
+button_3.addEventListener('mouseout', changeColorBack3);
+
+let top_nav_1 = document.getElementById('nav_1')
+
+function addDash() {
+    top_nav_1.style.textDecoration = 'underline';
+}
+
+function removeDash() {
+    top_nav_1.style.textDecoration = 'none';
+}
+
+top_nav_1.addEventListener('mouseover', addDash);
+top_nav_1.addEventListener('mouseout', removeDash);
+
+
+let top_nav_2 = document.getElementById('nav_2')
+
+function addDash2() {
+    top_nav_2.style.textDecoration = 'underline';
+}
+
+function removeDash2() {
+    top_nav_2.style.textDecoration = 'none';
+}
+
+top_nav_2.addEventListener('mouseover', addDash2);
+top_nav_2.addEventListener('mouseout', removeDash2);
+
+let top_nav_3 = document.getElementById('nav_3')
+
+function addDash3() {
+    top_nav_3.style.textDecoration = 'underline';
+}
+
+function removeDash3() {
+    top_nav_3.style.textDecoration = 'none';
+}
+
+top_nav_3.addEventListener('mouseover', addDash3);
+top_nav_3.addEventListener('mouseout', removeDash3);
+
+let top_nav_4 = document.getElementById('nav_4')
+
+function addDash4() {
+    top_nav_4.style.textDecoration = 'underline';
+}
+
+function removeDash4() {
+    top_nav_4.style.textDecoration = 'none';
+}
+
+top_nav_4.addEventListener('mouseover', addDash4);
+top_nav_4.addEventListener('mouseout', removeDash4);
+
+let top_nav_5 = document.getElementById('nav_5')
+
+function addDash5() {
+    top_nav_5.style.textDecoration = 'underline';
+}
+
+function removeDash5() {
+    top_nav_5.style.textDecoration = 'none';
+}
+
+top_nav_5.addEventListener('mouseover', addDash5);
+top_nav_5.addEventListener('mouseout', removeDash5);
